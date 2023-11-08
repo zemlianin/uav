8472a7889b9c0fa59053da9089571e1823a3eafb
zemlianin <detulie@mail.ru>
2023-11-08 15:48:59 +0300

    add_crop_files

--- dribnokhod_predict.py
+++ dribnokhod_predict.py
@@ -8,7 +8,11 @@ start_time = datetime.now()
 
 model.predict(source="test\\images\\youtube-33_jpg.rf.05783b47767c3a08450ffbb9fa131ec2.jpg")  # предсказать по изображению
 
-time.sleep(5)
+print(datetime.now() - start_time)
+
+start_time = datetime.now()
+
+model.predict(source="test\\images\\youtube-33_jpg.rf.05783b47767c3a08450ffbb9fa131ec2.jpg")  # предсказать по изображению
 
 print(datetime.now() - start_time)
 
--- legasy/preprocess_dataset.py
+++ legasy/preprocess_dataset.py
@@ -2,7 +2,10 @@ from PIL import Image
 import argparse
 import logging
 import os
+import glob
 
+req_width=512
+req_height=512
 
 def add_margin(pil_img, top: int, right: int, bottom: int, left: int, color):
     width, height = pil_img.size
@@ -20,7 +23,7 @@ def dynamic_crop(pil_img):
         edge_values = []
         for h in range(height):
             edge_values.append(sum(pil_img.getpixel((0, h))) / 3)
-        if max(edge_values) <= 5 or min(edge_values) >= 250 or \
+        if max(edge_values) <= 10 or min(edge_values) >= 245 or \
                 sum(edge_values) / len(edge_values) < 1 or sum(edge_values) / len(edge_values) > 254:
             pil_img = pil_img.crop((1, 0, width, height))
         else:
@@ -33,7 +36,7 @@ def dynamic_crop(pil_img):
         edge_values = []
         for h in range(height):
             edge_values.append(sum(pil_img.getpixel((width - 1, h))) / 3)
-        if max(edge_values) <= 5 or min(edge_values) >= 250 or \
+        if max(edge_values) <= 10 or min(edge_values) >= 245 or \
                 sum(edge_values) / len(edge_values) < 1 or sum(edge_values) / len(edge_values) > 254:
             pil_img = pil_img.crop((0, 0, width - 1, height))
         else:
@@ -46,7 +49,7 @@ def dynamic_crop(pil_img):
         edge_values = []
         for w in range(width):
             edge_values.append(sum(pil_img.getpixel((w, 0))) / 3)
-        if max(edge_values) <= 5 or min(edge_values) >= 250 or \
+        if max(edge_values) <= 10 or min(edge_values) >= 245 or \
                 sum(edge_values) / len(edge_values) < 1 or sum(edge_values) / len(edge_values) > 254:
             pil_img = pil_img.crop((0, 1, width, height))
         else:
@@ -59,12 +62,16 @@ def dynamic_crop(pil_img):
         edge_values = []
         for w in range(width):
             edge_values.append(sum(pil_img.getpixel((w, height - 1))) / 3)
-        if max(edge_values) <= 5 or min(edge_values) >= 250 or \
+        if max(edge_values) <= 10 or min(edge_values) >= 245 or \
                 sum(edge_values) / len(edge_values) < 1 or sum(edge_values) / len(edge_values) > 254:
             pil_img = pil_img.crop((0, 0, width, height - 1))
         else:
             break
         crop_bottom += 1
+
+    pil_img = pil_img.resize((min(pil_img.width, 512), min(pil_img.height, 512)))
+
+    print(f"{pil_img}, {crop_left}, {crop_right}, {crop_top}, {crop_bottom}")
 
     return pil_img, crop_left, crop_right, crop_top, crop_bottom
 
@@ -72,11 +79,11 @@ def dynamic_crop(pil_img):
 def dynamic_padding(pil_img):
     width, height = pil_img.size
 
-    if 640 > height and 640 > width:
-        pad_left = (640 - width) // 2
-        pad_right = 640 - width - pad_left
-        pad_top = (640 - height) // 2
-        pad_bottom = 640 - height - pad_top
+    if req_height > height and req_width > width:
+        pad_left = (req_width - width) // 2
+        pad_right = req_width - width - pad_left
+        pad_top = (req_height - height) // 2
+        pad_bottom = req_height - height - pad_top
     elif width > height:
         pad_left, pad_right = 0, 0
         pad_top = (width - height) // 2
@@ -86,7 +93,7 @@ def dynamic_padding(pil_img):
         pad_left = (height - width) // 2
         pad_right = height - width - pad_left
     else:
-        return pil_img
+        return pil_img, 0, 0, 0, 0
 
     padded_image = add_margin(
         pil_img, pad_top, pad_right, pad_bottom, pad_left, (0, 0, 0))
@@ -96,10 +103,18 @@ def dynamic_padding(pil_img):
 
 def process_directories(old_directory: str, new_directory: str) -> None:
     got = set()
-    for path in os.listdir(os.path.join(old_directory, 'images')):
+    
+    for path in glob.glob(os.path.join(old_directory, 'images') + '/*'):
         try:
-            im = Image.open(os.path.join(old_directory, 'images'))
-            im2, left_minus, right_minus, top_minus, bottom_minus = dynamic_crop(im)
+            im = Image.open(path)
+            print(path)
+            response = dynamic_crop(im)
+            print(response)
+            im2=response[0]
+            left_minus=response[1]
+            right_minus=response[2]
+            top_minus=response[3]
+            bottom_minus=response[4]
             # if im2.width <= 320 or im2.height <= 320:
                 # print(f'image {path} bad quality')
             file = path.split('.')[0]
@@ -107,10 +122,18 @@ def process_directories(old_directory: str, new_directory: str) -> None:
                 print(f'image {path} got')
             else:
                 got.add(file)
-            im2, left_plus, _, top_plus, _ = dynamic_padding(im2)
-            im2.save(os.path.join(new_directory, 'images'))
-            with open(os.path.splitext(os.path.join(os.path.join(old_directory, 'labels'), path))[0] + '.txt', 'r') as f:
-                with open(os.path.splitext(os.path.join(os.path.join(new_directory, 'labels'), path))[0] + '.txt', 'w') as f_w:
+            response=dynamic_padding(im2)
+            print(response)
+
+            im2, left_plus, t, top_plus, y = dynamic_padding(im2)
+
+            new_path=path.replace(old_directory, new_directory)
+            im2.save(new_path)
+            print(path)
+            name = path.replace("old_photoes\images\\","")
+            print(name)
+            with open(os.path.splitext(os.path.join(os.path.join(old_directory, 'labels'), name))[0] + '.txt', 'r') as f:
+                with open(os.path.splitext(os.path.join(os.path.join(new_directory, 'labels'), name))[0] + '.txt', 'w') as f_w:
                     lines = f.readlines()
                     new_lines = []
                     for line in lines:
@@ -147,15 +170,16 @@ def parse_args():
                         help="do not logging.info() messages",
                         action="store_true")
 
-def setup_logging(quiet_mode):
-    logging.basicConfig(level=logging.WARNING if quiet_mode else logging.INFO,
+def setup_logging():
+    logging.basicConfig(level=logging.WARNING,
                         format="%(message)s")
 
 
 def main() -> None:
-    args = parse_args()
-    setup_logging(args.quiet_mode)
-    process_directories(args.old_directory, args.new_directory)
+    #args = parse_args()
+   # print(args)
+    setup_logging()
+    process_directories("old_photoes", "new_photoes")
 
 
 main()
